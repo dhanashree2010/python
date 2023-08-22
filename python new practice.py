aa20327a7f0bbb06d8f9f66c84fb3190efe0c86c
dhanashree2010 <142867669+dhanashree2010@users.noreply.github.com>
2023-08-22 10:54:32 +0530

    python practice function codes

---
+++ python new practice.py
@@ -0,0 +1,37 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: C:\Users\student\Desktop\Dhanashree\python.py ============
+Enter fisrt number=20
+Enter second number=20
+the addition is 40
+>>> 
+= RESTART: C:\Users\student\Desktop\Dhanashree\python.py
+Enter fisrt number=30
+Enter second number=10
+the substraction is 40
+>>> 
+= RESTART: C:\Users\student\Desktop\Dhanashree\python.py
+Enter fisrt number=50
+Enter second number=10
+the substraction is 40
+>>> 
+= RESTART: C:\Users\student\Desktop\Dhanashree\python.py
+Enter fisrt number=4
+Enter second number=10
+the multiplication is 40
+>>> 
+= RESTART: C:\Users\student\Desktop\Dhanashree\python.py
+Enter fisrt number=10
+Enter second number=4
+the division is 2.5
+>>> 
+= RESTART: C:\Users\student\Desktop\Dhanashree\python.py
+Enter fisrt number=27082002
+Enter second number=20102004
+the addition is 1.3472289628437046
+>>> 
+= RESTART: C:\Users\student\Desktop\Dhanashree\python.py
+Enter fisrt number=27082002
+Enter second number=20102004
+the addition is 47184006
